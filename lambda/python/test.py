c8f23efe249a71e7424c3b9d47b5f1adb3d27858
Ashwani Jha <ashwanijha04@gmail.com>
2020-06-04 12:23:01 +0530

    Create test.py

---
+++ lambda/python/test.py
@@ -0,0 +1,42 @@
+import json
+import logging
+import signal
+from urllib2 import build_opener, HTTPHandler, Request
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
+
+def handler(event, context):
+    '''Handle Lambda event from AWS'''
+    # Setup alarm for remaining runtime minus a second
+    signal.alarm((context.get_remaining_time_in_millis() / 1000) - 1)
+    send_response(event, context, "SUCCESS", {"Message": "Resource creation successful!"})
+
+
+def send_response(event, context, response_status, response_data):
+    '''Send a resource manipulation status response to CloudFormation'''
+    response_body = json.dumps({
+        "Status": response_status,
+        "Reason": "See the details in CloudWatch Log Stream: " + context.log_stream_name,
+        "PhysicalResourceId": context.log_stream_name,
+        "StackId": event['StackId'],
+        "RequestId": event['RequestId'],
+        "LogicalResourceId": event['LogicalResourceId'],
+        "Data": response_data
+    })
+
+    LOGGER.info('ResponseURL: %s', event['ResponseURL'])
+    LOGGER.info('ResponseBody: %s', response_body)
+
+    opener = build_opener(HTTPHandler)
+    request = Request(event['ResponseURL'], data=response_body)
+    request.add_header('Content-Type', '')
+    request.add_header('Content-Length', len(response_body))
+    request.get_method = lambda: 'PUT'
+    response = opener.open(request)
+    LOGGER.info("Status code: %s", response.getcode())
+    LOGGER.info("Status message: %s", response.msg)
+
+    
+    
